b2425db0dbd8fdf1bce9b0cd7466348742e40bc8
Jimmy <jimminh@163.com>
2020-08-11 10:49:24 +0000

    syncbot

--- os/centos/scripts/installers/0helper-yum.sh
+++ os/centos/scripts/installers/0helper-yum.sh
@@ -13,7 +13,7 @@ egrep "^baseurl" -r /etc/yum.repos.d |egrep -i -v vault
 # Tencent Cloud
 if runon_tencentcloud;then
   echo "Run on Tencent Cloud"
-  sed -i 's|^#?\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirrors.tencentyun.com/centos/$releasever|g' /etc/yum.repos.d/*.repo
+  sed -i 's|^#\?\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirrors.tencentyun.com/centos/$releasever|g' /etc/yum.repos.d/*.repo
   yum install -y --nogpgcheck  epel-release
   sed -i 's|^#?baseurl=https://download.fedoraproject.org/pub/epel|baseurl=http://mirrors.tencentyun.com/epel|g' /etc/yum.repos.d/epel*
   sed -i 's|^metalink|#metalink|' /etc/yum.repos.d/epel*
@@ -23,9 +23,9 @@ fi
 
 if runon_aliyun;then
   echo "Run on Aliyun Cloud"
-  sed -i 's|^#?\baseurl=https\?://mirror.centos.org/centos/$releasever|baseurl=http://mirrors.cloud.aliyuncs.com/centos/$releasever|g' /etc/yum.repos.d/*.repo
+  sed -i 's|^#?\(baseurl.*\)mirror.centos.org/centos/$releasever|baseurl=http://mirrors.cloud.aliyuncs.com/centos/$releasever|g' /etc/yum.repos.d/*.repo
   yum install -y --nogpgcheck  epel-release
-  sed -i 's|^#?baseurl=https://download.fedoraproject.org/pub/epel|baseurl=https://mirrors.aliyun.com/epel|g' /etc/yum.repos.d/epel*
+  sed -i 's|^#?\(baseurl.*\)download.fedoraproject.org/pub/epel|baseurl=https://mirrors.aliyun.com/epel|g' /etc/yum.repos.d/epel*
   sed -i 's|^metalink|#metalink|' /etc/yum.repos.d/epel*
   exit 0
 fi
@@ -33,9 +33,9 @@ fi
 # Huawei Cloud
 if runon_huaweicloud;then
   echo "Run on Huawei Cloud"
-  sed -i 's|^#?\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirrors.myhuaweicloud.com/centos/$releasever|g' /etc/yum.repos.d/*.repo
+  sed -i 's|^#\?\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirrors.myhuaweicloud.com/centos/$releasever|g' /etc/yum.repos.d/*.repo
   yum install -y --nogpgcheck  epel-release
-  sed -i 's|^#?\(baseurl.*\)download.fedoraproject.org/pub/epel|\1mirrors.myhuaweicloud.com/epel|g' /etc/yum.repos.d/epel*
+  sed -i 's|^#\?\(baseurl.*\)download.fedoraproject.org/pub/epel|\1mirrors.myhuaweicloud.com/epel|g' /etc/yum.repos.d/epel*
   sed -i 's|^metalink|#metalink|' /etc/yum.repos.d/epel*
   exit 0
 fi
